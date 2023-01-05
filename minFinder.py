f98f8d48ffccdf0496fc389308a880aab3198284
kosPap1 <99680776+kosPap1@users.noreply.github.com>
2023-01-05 19:47:21 +0200

    Update minFinder.py

--- minFinder.py
+++ minFinder.py
@@ -2,7 +2,7 @@
 
 def minFinder(dataset, Country, Year, N_smallest):
     data = dataset.loc[(dataset['Country'] == Country) & (dataset['Year'] == Year)]
-    dataTemp = data['Percent'].div(gdpFinder(Country, Year))
+    dataTemp = data['Percent'].div(gdpFinder(Country, Year))*100
     data2 = data.drop('Percent', axis=1, inplace=False)
     output = pd.concat([data2, dataTemp], axis=1)
     print(output)
