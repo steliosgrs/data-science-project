ecbd7dc24afb492aa6f4ad96f9eadc86d3149d79
kosPap1 <99680776+kosPap1@users.noreply.github.com>
2023-01-05 19:36:47 +0200

    Update minFinder.py

--- minFinder.py
+++ minFinder.py
@@ -1,10 +1,10 @@
 # this function finds the N minimun of the dataset. 
 
-def minFinder(Country, Year, N_smallest):
-    data = dfActual.loc[(dfActual['Country'] == Country) & (dfActual['Year'] == Year)]
+def minFinder(dataset, Country, Year, N_smallest):
+    data = dataset.loc[(dataset['Country'] == Country) & (dataset['Year'] == Year)]
     dataTemp = data['Percent'].div(gdpFinder(Country, Year))
     data2 = data.drop('Percent', axis=1, inplace=False)
     output = pd.concat([data2, dataTemp], axis=1)
+    print(output)
+    print(data)
     return output.nsmallest(N_smallest, 'Percent')
-  
-  
